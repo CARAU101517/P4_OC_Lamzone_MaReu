8cc9f06ed5cebd9a1384c4872ca4b4506fe2da20
CARAU101517 <73470877+CARAU101517@users.noreply.github.com>
2021-04-02 11:54:59 +0200

    7th_Show Date and Time Pickers  in AddMeetingActivity

--- app/src/main/java/com/example/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/com/example/mareu/ui/AddMeetingActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,6 +20,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -36,17 +39,14 @@ import java.util.Locale;
 
 public class AddMeetingActivity extends AppCompatActivity {
 
-    public TextView mAddStartDate;
-    public TextView mAddStartTime;
-    public TextView mAddEndDate;
-    public TextView mAddEndTime;
+    public TextView mAddStartDateAndTime;
+    public TextView mAddEndDateAndTime;
     public TextView mAddRoom;
+        public TextView mEmails ;
     public TextInputLayout mSubject;
-    public TextView mEmails ;
     public TextInputEditText mSubjectInput;
     public Button mSaveMeetingBtn;
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy", Locale.FRANCE);
-    SimpleDateFormat simpleTimeFormat = new SimpleDateFormat("hh:mm", Locale.FRANCE);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy"+" - "+"hh:mm", Locale.FRANCE);
 
     MeetingApiService meetingApiService;
 
@@ -55,26 +55,29 @@ public class AddMeetingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meeting);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mAddStartDate = findViewById(R.id.add_start_date_meeting);
-        mAddStartTime = findViewById(R.id.add_start_time_meeting);
-        mAddEndDate = findViewById(R.id.add_end_date_meeting);
-        mAddEndTime = findViewById(R.id.add_end_time_meeting);
+
+        mAddStartDateAndTime = findViewById(R.id.add_start_date_and_time_meeting);
+        mAddEndDateAndTime = findViewById(R.id.add_end_date_and_time_meeting);
         mAddRoom = findViewById(R.id.add_meeting_room);
         mEmails = findViewById(R.id.participants);
         mSubject = findViewById(R.id.meeting_subjectLyt);
         mSubjectInput = findViewById(R.id.add_meeting_subject);
         mSaveMeetingBtn = findViewById(R.id.save_meeting);
+
         meetingApiService = DI.getMeetingApiService();
-        mAddStartDate.setOnClickListener(new View.OnClickListener() {
+
+        mAddStartDateAndTime.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                filterDateDialog(mAddStartDate);
-            }
+            public void onClick(View view) { startDateTimePickerDialog(mAddStartDateAndTime); }
         });
 
+        mAddEndDateAndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { endDateTimePickerDialog(mAddEndDateAndTime); }
+        });
     }
-
 
     public static void openAddMeetingActivity(FragmentActivity activity) {
         Intent addMeetingActivityIntent = new Intent(activity, AddMeetingActivity.class);
@@ -92,17 +95,17 @@ public class AddMeetingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void filterDateDialog(TextView t) {
-
+    public void startDateTimePickerDialog(TextView t) {
         // calender class's instance and get current date , month and year from calender
         final Calendar c = Calendar.getInstance(Locale.FRANCE);
         int mYear = c.get(Calendar.YEAR); // current year
         int mMonth = c.get(Calendar.MONTH); // current month
         int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        int mHour = c.get(Calendar.HOUR_OF_DAY); // current hour
+        int mMinute = c.get(Calendar.MINUTE); // current minute
         // date picker dialog
         DatePickerDialog datePickerDialog = new DatePickerDialog(this,
                 new DatePickerDialog.OnDateSetListener() {
-
                     @Override
                     public void onDateSet(DatePicker date, int year,
                                           int monthOfYear, int dayOfMonth) {
@@ -112,7 +115,60 @@ public class AddMeetingActivity extends AppCompatActivity {
                         t.setText(sdf.format(calendar.getTime()));
                     }
                 }, mYear, mMonth, mDay);
+
+                        // time picker dialog
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                Calendar timeCalendar = Calendar.getInstance();
+                                timeCalendar.set(i, i1);
+                                t.setText(sdf.format(timeCalendar.getTime()));
+                            }
+                                    }, mHour, mMinute, true);
+                        timePickerDialog.show();
+
+        datePickerDialog.show();
+    }
+
+    public void endDateTimePickerDialog(TextView textView) {
+        // calender class's instance and get current date , month and year from calender
+        final Calendar c = Calendar.getInstance(Locale.FRANCE);
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+        int mHour = c.get(Calendar.HOUR_OF_DAY); // current hour
+        int mMinute = c.get(Calendar.MINUTE); // current minute
+        // date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker date, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // set day of month , month and year value in the edit text
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+                        textView.setText(sdf.format(calendar.getTime()));
+                    }
+                }, mYear, mMonth, mDay);
+
+        // time picker dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        Calendar timeCalendar = Calendar.getInstance();
+                        timeCalendar.set(i, i1);
+                        textView.setText(sdf.format(timeCalendar.getTime()));
+                    }
+                }, mHour, mMinute, true);
+        timePickerDialog.show();
+
         datePickerDialog.show();
     }
 
 }
+
+
+
+
