73770f3f9568e8511050296609809edc4691bee2
CARAU101517 <73470877+CARAU101517@users.noreply.github.com>
2021-04-12 14:49:47 +0200

    10th_changes on AddRoomDialog and add autoComplete participantsEmails

--- app/src/main/java/com/example/mareu/ui/AddMeetingActivity.java
+++ app/src/main/java/com/example/mareu/ui/AddMeetingActivity.java
@@ -12,21 +12,31 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.provider.ContactsContract;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.mareu.R;
 import com.example.mareu.di.DI;
+import com.example.mareu.model.Employee;
+import com.example.mareu.model.Meeting;
+import com.example.mareu.service.DummyMeetingGenerator;
 import com.example.mareu.service.MeetingApiService;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.EnumMap;
+import java.util.List;
 import java.util.Locale;
 
 public class AddMeetingActivity extends AppCompatActivity {
@@ -34,13 +44,12 @@ public class AddMeetingActivity extends AppCompatActivity {
     public TextView mAddStartDateAndTime;
     public TextView mAddEndDateAndTime;
     public TextView mAddRoom;
-    public TextView mEmails;
+    public MultiAutoCompleteTextView mEmails;
     public TextInputLayout mSubject;
     public TextInputEditText mSubjectInput;
     public Button mSaveMeetingBtn;
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy" + " - " + "hh:mm", Locale.FRANCE);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy" + " - " + "HH:mm", Locale.FRANCE);
     final Calendar calendarBegin = Calendar.getInstance(Locale.FRANCE);
-
 
 
     MeetingApiService meetingApiService;
@@ -85,6 +94,17 @@ public class AddMeetingActivity extends AppCompatActivity {
                 addRoomDialog(mAddRoom);
             }
         });
+
+        mEmails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { addParticipants(mEmails);}
+        });
+
+        mSaveMeetingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { saveMeeting(); }
+        });
+
     }
 
     public static void openAddMeetingActivity(FragmentActivity activity) {
@@ -171,42 +191,46 @@ public class AddMeetingActivity extends AppCompatActivity {
         datePickerDialog.show();
     }
 
+
     public void addRoomDialog(TextView mAddRoom) {
-        String[] roomsList = {"Sydney", "New York", "London", "Rio", "Oslo", "Cape Town", "Vancouver", "Singapore", "Paris", "San Francisco"};
-        boolean[] isCheckedList = {false, false, false, false, false, false, false, false, false, false};
-
-
+        CharSequence[] roomList = new CharSequence[]{"Sydney", "New York", "London", "Rio", "Oslo", "Cape Town", "Vancouver", "Singapore", "Paris", "San Francisco"};
+        final CharSequence[] mRoomSelected = new CharSequence[]{""};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Sélectionnez votre salle");
-        builder.setMultiChoiceItems(roomsList, isCheckedList, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                isCheckedList[which] = isChecked;
-                mAddRoom.setText(isCheckedList.toString());
-            }
-
-        });
-
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setSingleChoiceItems(roomList, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+                mRoomSelected[0] = roomList[which];
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            mAddRoom.setText(mRoomSelected[0].toString());
+            dialog.dismiss();
         });
 
-
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
         Dialog dialog = builder.create();
         dialog.show();
 
     }
 
+
+    public void addParticipants(MultiAutoCompleteTextView mEmails){
+        String[] employeesEmails = {"alex@lamzone.com","amandine@lamzone.com","paul@lamzone.com", "antoine@lamzone.com", "viviane@lamzone.com", "justin@lamzone.com",
+                "gaelle@lamzone.com", "martin@lamzone.com", "luc@lamzone.com", "agathe@lamzone.com", "chris@lamzone.com"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, employeesEmails);
+        mEmails.setAdapter(adapter);
+        mEmails.setThreshold(1);
+        mEmails.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+    }
+
+
+
+
+    public void saveMeeting(){
+
+    }
 
 
 
